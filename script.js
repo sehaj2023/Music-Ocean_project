3150ac419ae92f09360e974bdd3ba5cd0bfed28e
sehaj2023 <125124375+sehaj2023@users.noreply.github.com>
2023-06-01 23:09:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome To Music Ocean");
+let songs = [
+    {songName:"sidhumoose wala", filepath: "song/1.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/2.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/3.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/4.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/5.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/6.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/7.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/8.mp3"},
+    {songName:"sidhumoose wala", filepath: "song/9.mp3"},
+]
